de34c3f81176449df693e359395ee6523aa4149f
Guilherme Brida <guilherme.brida@crearesistemas.com.br>
2023-07-18 14:33:26 -0300

    teste e actions

--- api_copiloto/js/env.js
+++ api_copiloto/js/env.js
@@ -1,7 +1,10 @@
 
 window.BRIDA_USER = 'guilhermebrida';
-window.BRIDA_TOKEN = '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';
+window.BRIDA_TOKEN = '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';
+// window.BRIDA_TOKEN = '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';
 // window.BRIDA_TOKEN = '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';
 // window.BRIDA_TOKEN = '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';
 // window.BRIDA_TOKEN = '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';
 // window.BRIDA_TOKEN = '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';
+
+
